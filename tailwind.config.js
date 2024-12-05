50666a90743042068d4da52b5ce15a00fc3df435
Joy Chandra Uday <joychandraud@gmail.com>
2024-12-05 19:50:55 +0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,29 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#15293E',
+        secondary: '#F57231',
+        accent: '#FFF000',
+        neutral: '#F5F5F5',
+        neutralop: '#212121',
+        basic: "#00008b"
+      },
+      fontFamily: {
+        sans: '"Open Sans", serif',
+        poppins: '"Poppins", sans-serif'
+      },
+      backgroundImage: {
+        'basic': "url('https://res.cloudinary.com/dklikxmpm/image/upload/v1733160830/image_cxnhyo.webp')"
+      }
+    }
   },
-  plugins: [
-    require('daisyui'),
-  ],
-}
-
+  plugins: [daisyui]
+};
