0f58b95cbe76ee48da531358fd1065ff27d5e26a
Joy Chandra Uday <joychandraud@gmail.com>
2024-12-07 12:05:07 +0600

    update

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -32,26 +32,30 @@ const footerLinks = [
 
 const Footer = () => {
   return (
-    <footer className="flex gap-4 bg-primary text-white p-10">
-      <aside className="basis-2/5 flex-col flex">
-        <Link to={'/'} className="w-48 invert">
+    <footer className="flex flex-wrap gap-8 bg-primary text-white p-8 md:p-10">
+      {/* Logo and description */}
+      <aside className="w-full md:w-2/5 flex flex-col gap-4">
+        <Link to={'/'} className="w-36 md:w-48 invert">
           <img src={logo} alt="Logo" />
         </Link>
-        <p className="text-gray-400 w-3/4">
+        <p className="text-gray-400">
           A platform designed to help individuals and teams grow their skills, build networks, and achieve success in personal and professional development.
         </p>
       </aside>
 
-      {footerLinks.map((section, index) => (
-        <div key={index} className="basis-1/5 flex-col gap-3 flex">
-          <h6 className="footer-title">{section.title}</h6>
-          {section.links.map((link, idx) => (
-            <Link key={idx} to={link.path} className="link link-hover">
-              {link.name}
-            </Link>
-          ))}
-        </div>
-      ))}
+      {/* Footer links */}
+      <div className="w-full md:w-3/5 flex flex-wrap gap-8">
+        {footerLinks.map((section, index) => (
+          <div key={index} className="w-1/2 md:w-1/3 flex flex-col gap-3">
+            <h6 className="footer-title text-lg font-semibold">{section.title}</h6>
+            {section.links.map((link, idx) => (
+              <Link key={idx} to={link.path} className="link link-hover text-gray-200">
+                {link.name}
+              </Link>
+            ))}
+          </div>
+        ))}
+      </div>
     </footer>
   );
 };
