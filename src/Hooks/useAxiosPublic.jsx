5afd2d88749704fd12f6200b162381d71baefe39
Joy Chandra Uday <joychandraud@gmail.com>
2025-06-05 00:33:00 +0600

    updated useAxios backen d link

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -3,8 +3,8 @@ import axios from 'axios';
 const useAxiosPublic = () => {
     const axiosInstance = axios.create({
         // baseURL: import.meta.env.API_URL
-        baseURL: 'http://localhost:3000'
-        // baseURL: 'https://talentgrowacademy-backend.vercel.app'
+        // baseURL: 'http://localhost:3000'
+        baseURL: 'https://talentgrowacademy-backend.vercel.app'
         // baseURL: 'https://talentgrowacademy-backend.onrender.com'
     });
 
